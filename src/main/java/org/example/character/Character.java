2c059abcf8c778f644f7dfcd4146675a3c54befe
javier <ertoyos@gmail.com>
2023-07-17 22:02:33 +0200

    add test and functions

--- src/main/java/org/example/character/Character.java
+++ src/main/java/org/example/character/Character.java
@@ -1,11 +1,12 @@
 package org.example.character;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Character {
-
-    private int health =1000;
+    private int health = 1000;
     private int level = 1;
     private boolean alive = true;
-
+    private Set<String> factions = new HashSet<>();
 
     public boolean isAlive() {
         return alive;
@@ -32,32 +33,88 @@ public class Character {
     }
 
     public int damage(int damage) {
-        this.health-= damage;
-        if(health <=0){
-            this.alive=false;
-            this.health=0;
+        this.health -= damage;
+        if (health <= 0) {
+            this.alive = false;
+            this.health = 0;
         }
-
+        return health;
     }
 
-    public int heal(int heal){
-        if(health > 0 && health <= 1000){
-            this.health+=heal;
-            if(health >1000){
+    public int heal(int heal) {
+        if (health > 0 && health < 1000) {
+            this.health += heal;
+            if (health >= 1000) {
                 setHealth(1000);
             }
         }
         return health;
     }
 
-    public int inflictDamage(Character character, int damage) {
-        if(target == this ){
-            return;
-        }
-        target.health -= damage;
-        if(target.health <= 0){
-            target.die();
+    public int inflictDamage(Character target, int damage) {
+        if (target == this || isAlly(target)) {
+            return 0;
         }
 
+        target.health -= damage;
+        if (target.health <= 0) {
+            target.alive = false;
+        }
+
+        return target.health;
+    }
+
+    public int autoHeal(Character target, int heal) {
+        if (target == this || isAlly(target)) {
+            target.heal(heal);
+        }
+        return target.health;
+    }
+
+    public void calculateDamage(Character target, Character attacker, int damage) {
+        if (isAlly(target)) {
+            return;
+        }
+        int damageModifier = (damage * 50) / 100;
+        if (target.getLevel() >= attacker.getLevel() * 5) {
+            damage -= damageModifier;
+            target.damage(damage);
+        } else if (target.getLevel() <= attacker.getLevel() * 5) {
+            damage += damageModifier;
+            target.damage(damage);
+        }
+    }
+
+    public void attackMelee(Character melee, double range, int damage) {
+        if (range > 2 || isAlly(melee)) {
+            return;
+        }
+        melee.damage(damage);
+    }
+
+    public void attackRanged(Character ranged, double range, int damage) {
+        if (range > 20 || isAlly(ranged)) {
+
+            return;
+        }
+        ranged.damage(damage);
+    }
+
+    public void joinFaction(String faction) {
+        factions.add(faction);
+    }
+
+    public void leaveFaction(String faction) {
+        factions.remove(faction);
+    }
+
+    public boolean isAlly(Character other) {
+        for (String faction : factions) {
+            if (other.factions.contains(faction)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
--- src/test/java/org/example/character/CharacterTest.java
+++ src/test/java/org/example/character/CharacterTest.java
@@ -1,6 +1,5 @@
 package org.example.character;
 
-import org.example.attacker.Attacker;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,78 +8,134 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CharacterTest {
 
     Character character = new Character();
-    Character character2 = new Character();
-    Attacker attacker = new Attacker();
 
     @Test
-    public void Health_should_starting_at_1000(){
+    public void Health_should_starting_at_1000() {
         assertEquals(1000, character.getHealth());
     }
 
     @Test
-    public void level_should_start_at_1(){
-        assertTrue(character.getLevel()>=1);
+    public void level_should_start_at_1() {
+        assertTrue(character.getLevel() >= 1);
     }
 
     @Test
-    public void character_should_start_alive(){
+    public void character_should_start_alive() {
         assertTrue(character.isAlive());
     }
 
     @Test
-    public void damage_is_subtracted_from_health(){
+    public void damage_is_subtracted_from_health() {
         character.damage(100);
-        assertEquals(900,character.getHealth());
+        assertEquals(900, character.getHealth());
     }
 
     @Test
-    public void when_damage_received_exceeds_current_health_character_dies(){
+    public void when_damage_received_exceeds_current_health_character_dies() {
         character.damage(1000);
-        assertEquals(0,character.getHealth());
+        assertEquals(0, character.getHealth());
     }
 
     @Test
-    public void if_damage_is_equal_to_health_character_dies(){
+    public void if_damage_is_equal_to_health_character_dies() {
         character.damage(1000);
         assertFalse(character.isAlive());
     }
 
     @Test
-    public void when_character_dies_health_becomes_0(){
+    public void when_character_dies_health_becomes_0() {
         character.damage(1100);
-        assertEquals(0,character.getHealth());
+        assertEquals(0, character.getHealth());
     }
 
     @Test
-    public void if_character_is_dead_cannot_be_healed(){
+    public void if_character_is_dead_cannot_be_healed() {
         character.damage(1000);
         character.heal(300);
         assertFalse(character.isAlive());
     }
 
     @Test
-    public void healing_cannot_raise_health_above_1000(){
+    public void healing_cannot_raise_health_above_1000() {
         character.heal(300);
-        assertEquals(1000,character.getHealth());
-    }
-
-    @Test
-    public void character_cannot_deal_damage_to_itself(){
-        character.inflictDamage(character,10);
         assertEquals(1000, character.getHealth());
     }
 
     @Test
-    public void character_can_only_heal_itself(){
-
+    public void character_cannot_deal_damage_to_itself() {
+        character.inflictDamage(character, 10);
+        assertEquals(1000, character.getHealth());
     }
 
     @Test
-    public void if_target_is_5_or_more_levels_above_the_attacker_Damage_is_reduced_by_50(){
-
+    public void character_can_only_heal_itself() {
+        character.autoHeal(character, 100);
+        assertEquals(1000, character.getHealth());
     }
 
+    @Test
+    public void target_is_5_or_more_Levels_above_the_attacker_Damage_is_reduced_by_50_percent() {
+        Character attacker = new Character();
+        character.setLevel(6);
+        attacker.setLevel(1);
+        attacker.calculateDamage(character, attacker, 400);
+        assertEquals(800, character.getHealth());
+    }
 
-
+    @Test
+    public void target_is_5_or_more_Levels_above_the_attacker_Damage_is_increased_by_50_percent() {
+        Character attacker = new Character();
+        character.setLevel(1);
+        attacker.setLevel(7);
+        attacker.calculateDamage(character, attacker, 200);
+        assertEquals(700, character.getHealth());
+    }
+    @Test
+    public void Melee_fighters_have_an_range_of_2_meters(){
+        Character melee = new Character();
+        melee.attackMelee(melee, 3.5, 100);
+        assertEquals(1000, melee.getHealth());
+    }
+    @Test
+    public void Ranged_fighters_have_a_range_of_meters(){
+        Character ranged = new Character();
+        ranged.attackRanged(ranged, 20.5, 100);
+        assertEquals(1000, ranged.getHealth());
+    }
+    @Test
+    public void charactersCanJoinAndLeaveFactions() {
+        character.joinFaction("Faction1");
+        character.joinFaction("Faction2");
+        assertTrue(character.isAlly(new Character() {{
+            joinFaction("Faction1");
+        }}));
+        assertTrue(character.isAlly(new Character() {{
+            joinFaction("Faction2");
+        }}));
+        character.leaveFaction("Faction1");
+        assertFalse(character.isAlly(new Character() {{
+            joinFaction("Faction1");
+        }}));
+        assertTrue(character.isAlly(new Character() {{
+            joinFaction("Faction2");
+        }}));
+    }
+    @Test
+    public void alliesCannotInflictDamageOnEachOther() {
+        Character attacker = new Character();
+        attacker.joinFaction("Faction1");
+        character.joinFaction("Faction1");
+        attacker.attackMelee(character, 1, 100);
+        assertEquals(1000, character.getHealth());
+    }
+    @Test
+    public void alliesCanHealEachOther() {
+        Character healer = new Character();
+        healer.joinFaction("Faction1");
+        healer.damage(100);
+        character.joinFaction("Faction1");
+        character.damage(100);
+        healer.autoHeal(character, 50);
+        assertEquals(950, character.getHealth());
+    }
 }
-
